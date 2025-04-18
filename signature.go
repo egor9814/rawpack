2a931d4aa69d8d893d692c7db3e6cfbe1132e7dc
Chalyh Egor <egor9814-original@yandex.ru>
2025-04-18 21:52:03 +0700

    remove signature's format flags

--- signature.go
+++ signature.go
@@ -1,56 +1,22 @@
 package rawpack
 
+import (
+	"bytes"
+)
+
 type FormatFlag byte
 
 const (
-	FormatNoFlags FormatFlag = 0
+	signaturePrefix = "RAW PACK FORMAT\u0000"
 )
-
-const (
-	formatFlagReserved1 FormatFlag = 1 << iota
-	formatFlagReserved2
-	formatFlagReserved3
-	formatFlagReserved4
-	formatFlagReserved5
-
-	formatFlagMask = formatFlagReserved5<<1 - 1
-)
-
-const (
-	signaturePrefix = "RAW PACK FORMAT"
-)
-
-func (f FormatFlag) IsValid() bool {
-	return f&formatFlagMask == f
-}
-
-func (f FormatFlag) toChar() byte {
-	return byte(f + '@')
-}
-
-func formatFromChar(b byte) FormatFlag {
-	return FormatFlag(b - '@')
-}
 
 type Signature [16]byte
 
-func (s Signature) Format() FormatFlag {
-	return formatFromChar(s[len(s)-1])
-}
-
-func (s *Signature) SetFormat(f FormatFlag) {
-	if !f.IsValid() {
-		panic("invalid format")
-	}
-	s[len(s)-1] = f.toChar()
-}
-
 func (s Signature) IsValid() bool {
-	return string(s[:len(s)-1]) == signaturePrefix && s.Format().IsValid()
+	return bytes.Equal(s[:], []byte(signaturePrefix))
 }
 
 func NewSignature() (s Signature) {
 	copy(s[:], signaturePrefix)
-	s.SetFormat(FormatNoFlags)
 	return
 }
